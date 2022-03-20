b78b36b63292cf00da8c3012baab1bdabd2f0bae
radoslav77 <rado7711@yahoo.co.uk>
2022-03-20 18:53:41 +0000

    fifth1 commit

---
+++ Qrcode/models.py
@@ -0,0 +1,19 @@
+from pyexpat import model
+from django.db import models
+
+
+# Create your models here.
+OUTLET = [
+    ('Room Service', 'Room Service'),
+    ('Thoe', 'Thoe Randal'),
+    ('Arch Bar', 'Arch Bar'),
+    ('Wellington Lounge', 'Wellington Lounge'),
+    ('Number One', 'Number One'),
+    ('BQT', 'BQT')
+]
+
+
+class GenarateCode(models.Model):
+    title = models.CharField(max_length=200)
+    outlet = models.CharField(max_length=200, choices=OUTLET)
+    img = models.ImageField(upload_to='media')
